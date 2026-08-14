d80b7cca127bf62181f5376b1b70b813ed1559cc
laomanda <panjaitanjakkob@gmail.com>
2026-08-14 12:49:31 +0700

    add dynamic sitemap.xml and update robots.txt for GSC

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,60 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Symfony\Component\Mime\MimeTypes;
+Route::get('/sitemap.xml', function () {
+    $baseUrl = 'https://ywdp.org';
+
+    $urls = [
+        ['loc' => $baseUrl . '/', 'changefreq' => 'daily', 'priority' => '1.0'],
+        ['loc' => $baseUrl . '/programs', 'changefreq' => 'daily', 'priority' => '0.9'],
+        ['loc' => $baseUrl . '/literasi', 'changefreq' => 'daily', 'priority' => '0.9'],
+        ['loc' => $baseUrl . '/tentang-kami', 'changefreq' => 'monthly', 'priority' => '0.7'],
+        ['loc' => $baseUrl . '/layanan', 'changefreq' => 'monthly', 'priority' => '0.7'],
+        ['loc' => $baseUrl . '/konsultasi', 'changefreq' => 'monthly', 'priority' => '0.7'],
+        ['loc' => $baseUrl . '/galeri-dpf', 'changefreq' => 'weekly', 'priority' => '0.7'],
+        ['loc' => $baseUrl . '/produk-mitra', 'changefreq' => 'weekly', 'priority' => '0.7'],
+    ];
+
+    try {
+        $articles = Article::published()->get(['slug', 'updated_at']);
+        foreach ($articles as $art) {
+            $urls[] = [
+                'loc' => $baseUrl . '/literasi/' . $art->slug,
+                'lastmod' => $art->updated_at?->toIso8601String(),
+                'changefreq' => 'weekly',
+                'priority' => '0.8',
+            ];
+        }
+
+        $programs = Program::active()->get(['slug', 'updated_at']);
+        foreach ($programs as $prog) {
+            $urls[] = [
+                'loc' => $baseUrl . '/programs/' . $prog->slug,
+                'lastmod' => $prog->updated_at?->toIso8601String(),
+                'changefreq' => 'weekly',
+                'priority' => '0.8',
+            ];
+        }
+    } catch (\Throwable $e) {
+        // Ignore DB issues safely
+    }
+
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    foreach ($urls as $u) {
+        $xml .= '<url>';
+        $xml .= '<loc>' . htmlspecialchars($u['loc']) . '</loc>';
+        if (!empty($u['lastmod'])) {
+            $xml .= '<lastmod>' . $u['lastmod'] . '</lastmod>';
+        }
+        $xml .= '<changefreq>' . $u['changefreq'] . '</changefreq>';
+        $xml .= '<priority>' . $u['priority'] . '</priority>';
+        $xml .= '</url>';
+    }
+    $xml .= '</urlset>';
+
+    return response($xml, 200, ['Content-Type' => 'application/xml']);
+});
 
 Route::get('/{path?}', function (?string $path = null) {
     $distPath = base_path('frontend-dpf/dist');
